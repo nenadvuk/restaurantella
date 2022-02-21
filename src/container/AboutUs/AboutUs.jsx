97c73c88e5a010fbf78fa37291374889ad34b1af
nenad_vuk <nenadvukasinovic@rs.gmail.com>
2022-02-21 18:10:16 +0100

    Menu

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -26,7 +26,7 @@ const AboutUs = () => (
       </div>
 
       <div className="app__aboutus-content_knife flex__center">
-        <img src={images.knife} alt="about_knife" />
+        <img src={images.forkAndKnife} alt="about_forkAndKnife" />
       </div>
 
       <div className="app__aboutus-content_history">
