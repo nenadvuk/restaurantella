09c66b1c778047f7fff56cf64995e5b3bc042f72
nenad_vuk <nenadvukasinovic@rs.gmail.com>
2022-02-22 23:20:07 +0100

    awards

--- src/constants/images.js
+++ src/constants/images.js
@@ -11,7 +11,7 @@ import overlaybg from '../assets/overlaybg.png';
 import spoon from '../assets/spoon.svg';
 import welcome from '../assets/welcome.png';
 import findus from '../assets/findus.png';
-import laurels from '../assets/laurels.png';
+import laurels from '../assets/laurels.jpg';
 import award01 from '../assets/award01.png';
 import award02 from '../assets/award02.png';
 import award03 from '../assets/award03.png';
